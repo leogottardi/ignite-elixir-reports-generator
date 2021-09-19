ea4fdfc755264bcbb60de45f88e56c91d14ea85c
Leonardo Gottardi <leomgottardi@gmail.com>
2021-09-19 19:55:04 -0300

    feat: adicionando modulo para formatar o arquivo csv em uma Lista

---
+++ lib/parser.ex
@@ -0,0 +1,38 @@
+defmodule GenReport.Parser do
+
+  @months %{
+    1 => "janeiro",
+    2 => "fevereiro",
+    3 => "março",
+    4 => "abril",
+    5 => "maio",
+    6 => "junho",
+    7 => "julho",
+    8 => "agosto",
+    9 => "setembro",
+    10 => "outubro",
+    11 => "novembro",
+    12 => "dezembro"
+  }
+
+  def parse_file(filename) do
+    filename
+    |> File.stream!()
+    |> Enum.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(&format_values/1)
+    |> List.update_at(3, &@months[&1])
+  end
+
+  defp format_values(string) do
+    case Integer.parse(string) do
+      {integer, _rest} -> integer
+      :error -> String.downcase(string)
+    end
+  end
+end
