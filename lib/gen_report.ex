6dd26b5f175a2542464423371f43f0686722cfea
Leonardo Gottardi <leomgottardi@gmail.com>
2021-09-20 21:10:49 -0300

    feat: alterando ponto para aspas em maps

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -11,17 +11,19 @@ defmodule GenReport do
     |> Enum.reduce(report_acc(), fn line, report -> handle_report(line, report) end)
   end
 
+  def build(), do: {:error, "Insira o nome de um arquivo"}
+
   defp handle_report([name, hours, _day, month, year], %{
-         all_hours: all_hours,
-         hours_per_month: persons_month,
-         hours_per_year: persons_year
+         "all_hours" => all_hours,
+         "hours_per_month" => persons_month,
+         "hours_per_year" => persons_year
        }) do
     all_hours = Map.put(all_hours, name, all_hours[name] + hours)
 
     hours_per_month = handle_sum_hours(persons_month, name, month, hours)
     hours_per_year = handle_sum_hours(persons_year, name, year, hours)
 
-    %{all_hours: all_hours, hours_per_month: hours_per_month, hours_per_year: hours_per_year}
+    %{"all_hours" => all_hours, "hours_per_month" => hours_per_month, "hours_per_year" => hours_per_year}
   end
 
   defp handle_sum_hours(persons, name, month, hours) do
@@ -35,7 +37,7 @@ defmodule GenReport do
     hours_per_month = build_hours_per_data(@months)
     hours_per_year = build_hours_per_data(@years)
 
-    %{all_hours: all_hours, hours_per_month: hours_per_month, hours_per_year: hours_per_year}
+    %{"all_hours"  => all_hours, "hours_per_month"  => hours_per_month, "hours_per_year"  => hours_per_year}
   end
 
   defp build_hours_per_data(data), do: Enum.into(@persons, %{}, &{&1, build_per_data(data)})
