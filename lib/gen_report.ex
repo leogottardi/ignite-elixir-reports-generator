f5bab4825a46f07e99194a7ebe607996d04bf55a
Leonardo Gottardi <leomgottardi@gmail.com>
2021-09-20 21:04:26 -0300

    feat: abstraindo funcoes

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -5,9 +5,7 @@ defmodule GenReport do
   @months Data.build("months")
   @years Data.build("years")
 
-
   def build(filename) do
-
     filename
     |> Parser.parse_file()
     |> Enum.reduce(report_acc(), fn line, report -> handle_report(line, report) end)
@@ -20,19 +18,13 @@ defmodule GenReport do
        }) do
     all_hours = Map.put(all_hours, name, all_hours[name] + hours)
 
-    hours_per_month = handle_hours_per_month(persons_month, name, month, hours)
-    hours_per_year = handle_hours_per_year(persons_year, name, year, hours)
+    hours_per_month = handle_sum_hours(persons_month, name, month, hours)
+    hours_per_year = handle_sum_hours(persons_year, name, year, hours)
 
     %{all_hours: all_hours, hours_per_month: hours_per_month, hours_per_year: hours_per_year}
   end
 
-  defp handle_hours_per_year(persons, name, year, hours) do
-      person = %{name => Map.put(persons[name], year, persons[name][year] + hours)}
-
-      Map.merge(persons, person)
-  end
-
-  defp handle_hours_per_month(persons, name, month, hours) do
+  defp handle_sum_hours(persons, name, month, hours) do
     person = %{name => Map.put(persons[name], month, persons[name][month] + hours)}
 
     Map.merge(persons, person)
@@ -40,12 +32,12 @@ defmodule GenReport do
 
   def report_acc() do
     all_hours = Enum.into(@persons, %{}, &{&1, 0})
-    hours_per_month = Enum.into(@persons, %{}, &{&1, months_per_person()})
-    hours_per_year = Enum.into(@persons, %{}, &{&1, years_per_person()})
+    hours_per_month = build_hours_per_data(@months)
+    hours_per_year = build_hours_per_data(@years)
 
     %{all_hours: all_hours, hours_per_month: hours_per_month, hours_per_year: hours_per_year}
   end
 
-  defp months_per_person(), do: Enum.into(@months, %{}, &{&1, 0})
-  defp years_per_person(), do: Enum.into(@years, %{}, &{&1, 0})
+  defp build_hours_per_data(data), do: Enum.into(@persons, %{}, &{&1, build_per_data(data)})
+  defp build_per_data(data), do: Enum.into(data, %{}, &{&1, 0})
 end
